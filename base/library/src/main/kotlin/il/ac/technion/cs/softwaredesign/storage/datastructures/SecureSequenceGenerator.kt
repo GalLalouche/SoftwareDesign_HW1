29903f6b889be70c51d62672e0f986f243503388
aviadshiber <aviadshiber@campus.technion.ac.il>
2019-05-17 13:25:35 +0300

    injecting from constructor instead of field

--- base/library/src/main/kotlin/il/ac/technion/cs/softwaredesign/storage/datastructures/SecureSequenceGenerator.kt
+++ base/library/src/main/kotlin/il/ac/technion/cs/softwaredesign/storage/datastructures/SecureSequenceGenerator.kt
@@ -7,16 +7,15 @@ import il.ac.technion.cs.softwaredesign.storage.utils.TREE_CONST
 import javax.inject.Inject
 import javax.inject.Singleton
 
-@Target(AnnotationTarget.FIELD)
+@Target(AnnotationTarget.CONSTRUCTOR)
 @Retention(AnnotationRetention.RUNTIME)
 @BindingAnnotation
 annotation class GeneratorStorage
 
 @Singleton
-class SecureSequenceGenerator :ISequenceGenerator {
-    private val lastGeneratedKey="LAST_GENERATED_ID".toByteArray() //maybe should be hashed or something
-    @Inject @GeneratorStorage
-    private lateinit var secureStorage:SecureStorage
+class SecureSequenceGenerator
+@Inject @GeneratorStorage constructor(private val secureStorage: SecureStorage) :ISequenceGenerator {
+    private val lastGeneratedKey="LAST_GENERATED_ID".toByteArray()
     override fun next(): Long {
         val currentValueInByteArray :ByteArray= secureStorage.read(lastGeneratedKey) ?:  ByteUtils.longToBytes(TREE_CONST.ROOT_INIT_INDEX)
         val nextValue:Long= ByteUtils.bytesToLong(currentValueInByteArray)+1L
