a7ef1d453e6c5950e7741b1d1f06e51c38d42777
aviadshiber <aviadshiber@campus.technion.ac.il>
2019-05-19 00:23:30 +0300

    init pointer and key in avl

---
+++ base/library/src/test/kotlin/il/ac/technion/cs/softwaredesign/storage/datastructures/SecureAVLTreeTest.kt
@@ -0,0 +1,155 @@
+package il.ac.technion.cs.softwaredesign.storage.datastructures
+
+import com.google.common.primitives.Longs
+import il.ac.technion.cs.softwaredesign.storage.ISecureStorageKey
+import il.ac.technion.cs.softwaredesign.storage.SecureStorage
+import il.ac.technion.cs.softwaredesign.storage.utils.TREE_CONST.ROOT_INIT_INDEX
+import il.ac.technion.cs.softwaredesign.storage.utils.TREE_CONST.ROOT_KEY
+import il.ac.technion.cs.softwaredesign.tests.assertWithTimeout
+import il.ac.technion.cs.softwaredesign.tests.isTrue
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class SecureAVLTreeTest {
+
+    data class SimpleKey(var i:Long): ISecureStorageKey<SimpleKey>{
+        override fun compareTo(other: SimpleKey): Int {
+           return i.compareTo(other.i)
+        }
+
+        override fun toByteArray(): ByteArray {
+            return Longs.toByteArray(this.i)
+        }
+
+        override fun fromByteArray(value: ByteArray?) {
+           this.i=Longs.fromByteArray(value)
+        }
+
+    }
+
+    class ByteArrayKey(private val bytes: ByteArray) {
+        override fun equals(other: Any?): Boolean =
+                this === other || other is ByteArrayKey && this.bytes contentEquals other.bytes
+        override fun hashCode(): Int = bytes.contentHashCode()
+        override fun toString(): String = bytes.contentToString()
+    }
+    private val storageMock= mutableMapOf<ByteArrayKey,ByteArray>()
+    private var storageLayer: SecureStorage = mockk()
+    private val tree=SecureAVLTree(storageLayer) {SimpleKey(0L)}
+    private val blackRedTree=TreeMap<SimpleKey,SimpleKey>()
+
+    private fun initTree() {
+
+        storageLayer.write(ROOT_KEY.toByteArray(), Longs.toByteArray(ROOT_INIT_INDEX))
+    }
+    init{
+        val keySlot= slot<ByteArray>()
+        val valueSlot= slot<ByteArray>()
+        every{
+            storageLayer.write(capture(keySlot),capture(valueSlot))
+        } answers {
+            val value  =valueSlot.captured
+            val key= ByteArrayKey(keySlot.captured)
+            storageMock[key]=value
+        }
+        every {
+            storageLayer.read(capture(keySlot))
+        } answers {
+            val key = ByteArrayKey(keySlot.captured)
+            storageMock[key]
+        }
+
+    }
+
+    @BeforeEach
+    fun setUp(){
+        initTree()
+
+    }
+
+
+    @Test
+    fun isEmpty() {
+        assertWithTimeout({ tree.size()==blackRedTree.size  }, isTrue)
+
+    }
+
+    @Test
+    fun size() {
+
+    }
+
+    @Test
+    fun height() {
+    }
+
+    @Test
+    fun get() {
+    }
+
+    @Test
+    fun contains() {
+    }
+
+    @Test
+    fun put() {
+        val value=SimpleKey(7)
+        tree.put(value)
+        blackRedTree[value] = value
+        assertWithTimeout({ tree[value] == blackRedTree[value] }, isTrue)
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun deleteMin() {
+    }
+
+    @Test
+    fun deleteMax() {
+    }
+
+    @Test
+    fun min() {
+    }
+
+    @Test
+    fun max() {
+    }
+
+    @Test
+    fun floor() {
+    }
+
+    @Test
+    fun ceiling() {
+    }
+
+    @Test
+    fun select() {
+    }
+
+    @Test
+    fun rank() {
+    }
+
+    @Test
+    fun keys() {
+    }
+
+    @Test
+    fun keysInOrder() {
+    }
+
+    @Test
+    fun keysLevelOrder() {
+    }
+
+
+}
