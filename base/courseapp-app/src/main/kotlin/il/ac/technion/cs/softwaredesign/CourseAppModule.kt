a5c27d5a709314cfbefb779c444dc01fcbbc3a57
aviadshiber <aviadshiber@campus.technion.ac.il>
2019-05-18 13:05:12 +0300

    updating module

--- base/courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseAppModule.kt
+++ base/courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseAppModule.kt
@@ -1,9 +1,17 @@
 package il.ac.technion.cs.softwaredesign
 
 import com.authzee.kotlinguice4.KotlinModule
+import il.ac.technion.cs.softwaredesign.storage.SecureStorage
+import il.ac.technion.cs.softwaredesign.storage.SecureStorageFactory
+import il.ac.technion.cs.softwaredesign.storage.impl.SecureStorageFactoryImpl
+import il.ac.technion.cs.softwaredesign.storage.impl.SecureStorageImpl
 
 class CourseAppModule : KotlinModule() {
     override fun configure() {
-        TODO("Implement me!")
+        bind<SecureStorage>().to<SecureStorageImpl>()
+        bind<SecureStorageFactory>().to<SecureStorageFactoryImpl>()
+        bind<IStorageLayer>().to<StorageLayer>()
+        bind<CourseAppInitializer>().to<CourseAppInitializerImpl>()
+        bind<CourseApp>().to<CourseAppImpl>()
     }
 }
