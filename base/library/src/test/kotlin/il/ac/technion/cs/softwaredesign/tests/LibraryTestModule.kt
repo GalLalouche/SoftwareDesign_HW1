f68025a02991139d1910cc77af4b9c3fc23db127
aviadshiber <aviadshiber@campus.technion.ac.il>
2019-05-19 18:42:12 +0300

    adding stupid tests for token manager

---
+++ base/library/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/LibraryTestModule.kt
@@ -0,0 +1,28 @@
+package il.ac.technion.cs.softwaredesign.tests
+
+import com.authzee.kotlinguice4.KotlinModule
+import il.ac.technion.cs.softwaredesign.managers.*
+import il.ac.technion.cs.softwaredesign.storage.ISequenceGenerator
+import il.ac.technion.cs.softwaredesign.storage.SecureStorage
+import il.ac.technion.cs.softwaredesign.storage.channels.IChannelStorage
+import il.ac.technion.cs.softwaredesign.storage.channels.SecureChannelStorage
+import il.ac.technion.cs.softwaredesign.storage.datastructures.GeneratorStorage
+import il.ac.technion.cs.softwaredesign.storage.impl.SecureStorageImpl
+import il.ac.technion.cs.softwaredesign.storage.statistics.IStatisticsStorage
+import il.ac.technion.cs.softwaredesign.storage.statistics.SecureStatisticsStorage
+import il.ac.technion.cs.softwaredesign.storage.users.IUserStorage
+import il.ac.technion.cs.softwaredesign.storage.users.SecureUserStorage
+
+
+class LibraryTestModule : KotlinModule() {
+    override fun configure() {
+        bind<IStatisticsStorage>().to<SecureStatisticsStorage>()
+        bind<IChannelStorage>().to<SecureChannelStorage>()
+        bind<ISequenceGenerator>().annotatedWith<UserIdSeqGenerator>().to<UserIdGenerator>()
+        bind<ISequenceGenerator>().annotatedWith<ChannelIdSeqGenerator>().to<ChannelIdGenerator>()
+        bind<IUserStorage>().to<SecureUserStorage>()
+        bind<ITokenManager>().to<TokenManager>()
+        bind<IUserManager>().to<UserManager>()
+        bind<IChannelManager>().to<ChannelManager>()
+    }
+}
--- base/library/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/TokenManagerTest.kt
+++ base/library/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/TokenManagerTest.kt
@@ -1,105 +1,103 @@
-//package il.ac.technion.cs.softwaredesign.tests
-//
-//import com.natpryce.hamkrest.equalTo
-//import il.ac.technion.cs.softwaredesign.*
-//import il.ac.technion.cs.softwaredesign.managers.TokenManager
-//import il.ac.technion.cs.softwaredesign.managers.TokenManager.Companion.INVALID_USERNAME
-//import io.mockk.every
-//import io.mockk.mockk
-//import org.junit.jupiter.api.Test
-//import org.junit.jupiter.api.TestInstance
-//
-//@TestInstance(TestInstance.Lifecycle.PER_METHOD)
-//class TokenManagerTest {
-//    companion object {
-//        var users : ArrayList<User> = ArrayList()
-//        init {
-//            val username = "user"
-//            val pwd = "pwd"
-//            val token = "token"
-//            for (i in 0..9) {
-//                val num = i.toString()
-//                val usernameToAdd = username + num
-//                val pwdToAdd = pwd + num
-//                val tokenToAdd = token + num + num + num
-//                val user = User(usernameToAdd, pwdToAdd, tokenToAdd)
-//                users.add(user)
-//            }
-//        }
-//    }
-//
-//    private val storageLayer = mockk<IStorageLayer>()
-//
-//    /**
-//     * isTokenValid
-//     */
-//    @Test
-//    fun `token that has'nt been written to the system is not exist`() {
-//        every { storageLayer.readUsernameOfToken(any()) } returns null
-//        every { storageLayer.readUsernameOfToken(users[1].token) } returns users[1].userName
-//        val tokenManager = TokenManager(storageLayer)
-//        assertWithTimeout({ tokenManager.isTokenValid(users[0].token) }, isFalse)
-//    }
-//
-//    @Test
-//    fun `token that has been written to the system is exist`() {
-//        every { storageLayer.readUsernameOfToken(any()) } returns null
-//        every { storageLayer.readUsernameOfToken(users[0].token) } returns users[0].userName
-//        val tokenManager = TokenManager(storageLayer)
-//        assertWithTimeout({ tokenManager.isTokenValid(users[0].token) }, isTrue)
-//    }
-//
-//    @Test
-//    fun `invalid token that has been written to the system is not exist`() {
-//        every { storageLayer.readUsernameOfToken(any()) } returns null
-//        every { storageLayer.readUsernameOfToken(users[0].token) } returns INVALID_USERNAME
-//        val tokenManager = TokenManager(storageLayer)
-//        assertWithTimeout({ tokenManager.isTokenValid(users[0].token) }, isFalse)
-//    }
-//
-//    /**
-//     * getUserIdByToken
-//     */
-//    @Test
-//    fun `returned user compatible to written token-user mapping`() {
-//        every { storageLayer.readUsernameOfToken(any()) } returns null
-//        every { storageLayer.readUsernameOfToken(users[0].token) } returns users[0].userName
-//        val tokenManager = TokenManager(storageLayer)
-//        assertWithTimeout({ tokenManager.getUserIdByToken(users[0].token) }, equalTo(users[0].userName))
-//    }
-//
-//    @Test
-//    fun `null user throws`() {
-//        every { storageLayer.readUsernameOfToken(any()) } returns null
-//        every { storageLayer.readUsernameOfToken(users[1].token) } returns users[0].userName
-//        val tokenManager = TokenManager(storageLayer)
-//        assertThrowsWithTimeout<String, IllegalArgumentException>({ tokenManager.getUserIdByToken(users[0].userName) })
-//    }
-//
-//    @Test
-//    fun `invalid username user throws`() {
-//        every { storageLayer.readUsernameOfToken(any()) } returns null
-//        every { storageLayer.readUsernameOfToken(users[0].token) } returns INVALID_USERNAME
-//        val tokenManager = TokenManager(storageLayer)
-//        assertThrowsWithTimeout<String, IllegalArgumentException>({ tokenManager.getUserIdByToken(users[0].userName) })
-//    }
-//
-//    /**
-//     * assignTokenToUserId
-//     */
-//    @Test
-//    fun `invalid username throws`() {
-//        val tokenManager = TokenManager(storageLayer)
-//        assertThrowsWithTimeout<String, IllegalArgumentException>({ tokenManager.assignTokenToUserId(INVALID_USERNAME) })
-//    }
-//
-//    /**
-//     * invalidateUserToken
-//     */
-//    @Test
-//    fun `invalid mapped username throws`() {
-//        every { storageLayer.readUsernameOfToken(any()) } returns INVALID_USERNAME
-//        val tokenManager = TokenManager(storageLayer)
-//        assertThrowsWithTimeout<Unit, IllegalArgumentException>({ tokenManager.invalidateUserToken(users[0].token) })
-//    }
-//}
+package il.ac.technion.cs.softwaredesign.tests
+
+import com.authzee.kotlinguice4.getInstance
+import com.google.inject.Guice
+import com.google.inject.Inject
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import il.ac.technion.cs.softwaredesign.*
+import il.ac.technion.cs.softwaredesign.managers.ITokenManager
+import il.ac.technion.cs.softwaredesign.managers.TokenManager
+import il.ac.technion.cs.softwaredesign.storage.users.IUserStorage
+import il.ac.technion.cs.softwaredesign.storage.utils.MANAGERS_CONSTS.INVALID_USER_ID
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+class TokenManagerTest{
+
+    private val userStorage = mockk<IUserStorage>()
+    private val tokenManager=TokenManager(userStorage)
+
+    /**
+     * isTokenValid
+     */
+    @Test
+    fun `token that has'nt been written to the system is not exist`() {
+        every{userStorage.getUserIdByToken(any()) } returns null
+        assertWithTimeout({ tokenManager.isTokenValid("InvalidToken")}, isFalse)
+    }
+
+    @Test
+    fun `token that has been written to the system is exist`() {
+        every { userStorage.getUserIdByToken("validToken") } returns 5L
+        assertWithTimeout({ tokenManager.isTokenValid("validToken") }, isTrue)
+    }
+
+
+    /**
+     * getUserIdByToken
+     */
+    @Test
+    fun `returned user compatible to written token-user mapping`() {
+        every{ userStorage.getUserIdByToken("aviad")} returns 10L
+        assertWithTimeout({ tokenManager.getUserIdByToken("aviad") }, equalTo(10L))
+    }
+
+    /**
+     * getUserIdByToken
+     */
+    @Test
+    fun `getUserIdByToken returns null if token is not valid`() {
+        Assertions.assertNull( tokenManager.getUserIdByToken("aviad"))
+    }
+
+    @Test
+    fun `token been assigned to user`() {
+        every { userStorage.setUserIdToToken(any(),1L) }
+        val token=tokenManager.assignTokenToUserId(1L)
+        every{ userStorage.getUserIdByToken(token)} returns 1L
+        assertWithTimeout({tokenManager.isTokenValid(token)}, isTrue)
+    }
+
+    @Test
+    fun `token been invalidated`() {
+       every { userStorage.setUserIdToToken(any(),any()) } answers {  }
+        every{ userStorage.getUserIdByToken(any())} returns 1L
+        val token=tokenManager.assignTokenToUserId(1L)
+        assertWithTimeout({tokenManager.isTokenValid(token)}, isTrue)
+        tokenManager.invalidateUserToken(token)
+        every{ userStorage.getUserIdByToken(token)} returns INVALID_USER_ID
+        assertWithTimeout({tokenManager.isTokenValid(token)}, isFalse)
+    }
+/*
+    @Test
+    fun `invalid username user throws`() {
+        every { userStorage.readUsernameOfToken(any()) } returns null
+        every { userStorage.readUsernameOfToken(users[0].token) } returns INVALID_USERNAME
+        val tokenManager = TokenManager(userStorage)
+        assertThrowsWithTimeout<String, IllegalArgumentException>({ tokenManager.getUserIdByToken(users[0].userName) })
+    }
+
+    *//**
+     * assignTokenToUserId
+     *//*
+    @Test
+    fun `invalid username throws`() {
+        val tokenManager = TokenManager(userStorage)
+        assertThrowsWithTimeout<String, IllegalArgumentException>({ tokenManager.assignTokenToUserId(INVALID_USERNAME) })
+    }
+
+    *//**
+     * invalidateUserToken
+     *//*
+    @Test
+    fun `invalid mapped username throws`() {
+        every { userStorage.readUsernameOfToken(any()) } returns INVALID_USERNAME
+        val tokenManager = TokenManager(userStorage)
+        assertThrowsWithTimeout<Unit, IllegalArgumentException>({ tokenManager.invalidateUserToken(users[0].token) })
+    }*/
+}
